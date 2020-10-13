1366707cb51095935c830f86ceb43d4436414ea0
Paul Wilkerson <wilkerson-26-2002@outlook.com>
2020-10-13 18:33:15 -0500

    Adding the finishing touches

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('blog/', views.blog, name='blog'),
+    path('', views.blog, name='blog'),
     path('about/', views.about, name='about'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
 ]
