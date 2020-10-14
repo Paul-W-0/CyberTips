628e1f387b24978446bbf09778f32105117a0162
Paul Wilkerson <wilkerson-26-2002@outlook.com>
2020-10-13 19:11:01 -0500

    Removed some unused code

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,9 @@
 from django.db import models
-STATUS = (
-    ('draft', 'Draft'),
-    ('published', 'Published'),
-
-)
 class Post(models.Model):
     title = models.CharField(max_length=150)
     post_body = models.TextField()
     slug = models.SlugField(max_length=150, unique=True)
     published_on = models.DateTimeField(auto_now_add=True)
-    status_choices = models.CharField(max_length=10, choices=STATUS, default='draft')
 class Meta:
-    ordering = ('published_on',)
     def __str__(self):
         return self.title()
